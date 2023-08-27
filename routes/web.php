88a3a38b85a99ce4a8fccc3e164067ccf6a4d2e9
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-08-27 14:29:07 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExerciceController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', [ExerciceController::class, 'index']);
+Route::get('/resume', [ExerciceController::class, 'resume']);
+Route::get('/projects', [ExerciceController::class, 'projects']);
+Route::get('/contact', [ExerciceController::class, 'contact']);
