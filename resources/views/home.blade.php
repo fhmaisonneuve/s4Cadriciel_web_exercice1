2c4b9790ef6e3631d9e669458cbafea3965f2bae
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-08-28 14:52:16 -0400

    correction home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,9 +16,9 @@
                         <div class="fs-3 fw-light text-muted">Je peux être le facilitateur pour toute programmation</div>
                         <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline"> front-end et backend</span></h2>
                         <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
-                            <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.html">CV</a>
+                            <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume">CV</a>
                             <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder"
-                                href="projects.html">Projects</a>
+                                href="projects">Projects</a>
                         </div>
                     </div>
                 </div>
