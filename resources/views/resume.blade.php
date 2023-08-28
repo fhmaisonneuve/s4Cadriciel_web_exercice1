3048c827bb62499364ee9d80abc4621d9f53972c
fhmaisonneuve <e2296133@cmaisonnuve.qc.ca>
2023-08-28 14:42:11 -0400

    Typo dans le CV

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -375,6 +375,9 @@
                                     <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Adobe
                                         Software Suite</div>
                                 </div>
+                                <div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Programmation</div>
+                                </div>
 
                             </div>
                         </div>
